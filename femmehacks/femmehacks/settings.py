f03e1cdf5350953de3ee118ca66d08d03a6b614d
Lanting Chiang <49918461+lantingchiang@users.noreply.github.com>
2020-02-08 10:56:38 -0500

    set up models

--- femmehacks/femmehacks/settings.py
+++ femmehacks/femmehacks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'forum.apps.ForumConfig',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'forum.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- femmehacks/forum/models.py
+++ femmehacks/forum/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+# reference user model by settings.AUTH_USER_MODEL
+from django.conf import settings
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+    # optional fields listing user's profession/field
+    profession = models.CharField(max_length=200, blank=True)
+    field = models.CharField(max_length=200, blank=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['password']
+
+    def __str__(self):
+        return self.username
+
+
+class Post(models.Model):
+    def allow_comment_default():
+        return False   
+
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=400)
+    description = models.TextField()
+    allow_comments = models.BooleanField(default=allow_comment_default())
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    # also delete this comment when the corresponding post is deleted
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
