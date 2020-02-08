f872826fc27e8076b2fec2ff9cebe643848d69a5
Lanting Chiang <49918461+lantingchiang@users.noreply.github.com>
2020-02-08 11:36:03 -0500

    add urls

--- femmehacks/femmehacks/urls.py
+++ femmehacks/femmehacks/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .forum import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', views.home, name='home'),
+    path('/create-post/', views.create_post, name='create-post'),
+    path('/view-post/', views.view_post, name='view-post'),
+    path('/accounts/login', views.login, name='login'),
+    path('/accounts/logout', views.logout, name='logout'),
+    path('/accounts/signup', views.signup, name='signup'),
+    path('/admin/', admin.site.urls),
 ]
--- femmehacks/forum/views.py
+++ femmehacks/forum/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+#homepage
+def home(request):
+    return HttpResponse("Homepage")
+
+#posting page
+def create_post(request):
+    return HttpResponse("create_psot")
+
+#viewing post page
+def view_post(request):
+    return HttpResponse("view_post")
+
+#login
+def login(request):
+    return HttpResponse("login")
+
+#logout
+def logout(request):
+    return HttpResponse("logout")
+
+#signup
+def signup(request):
+    return HttpResponse("signup")
+
+
